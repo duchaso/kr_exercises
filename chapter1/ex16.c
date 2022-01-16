48783d4839695cc97d297f63f63f84b7d212d79b
Danyl Uchen <uchendanyl@gmail.com>
2022-01-16 16:17:41 +0200

    ex16 correct ver

--- chapter1/ex16.c
+++ chapter1/ex16.c
@@ -1,7 +1,7 @@
 //Revise the main routine of the longest-line program so it will correctly print the length of arbitrarily long input lines, and as much as possible of the text. 
 
 #include <stdio.h>
-#define MAXLINE 10
+#define MAXLINE 5 
 
 int getl(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -25,20 +25,31 @@ int main()
     printf("Len: %d\n", max);
     return 0;
 }
-//qqqqqqqq\n\0
+
 int getl(char s[], int lim)
 {
     int c, i;
 
     for(i = 0; i<lim-1 && (c=getchar()) != EOF && c!='\n'; ++i)
-        s[i] = c;
-    while(c=getchar() != EOF && c!='\n')
-        ++i;
-    if(c == '\n')
     {
-        s[lim-2] = c;
+        s[i] = c;
     }
-    s[lim-1] = '\0';
+    if(i < lim-1)
+    {
+        if(c == '\n')
+        {
+            s[i] = c;
+            ++i;
+        }
+        s[i] = '\0';
+        return i;
+    } else {
+        s[lim-2] = '\n';
+        s[lim-1] = '\0';
+        while((c=getchar()) != EOF && c!='\n')
+            ++i;
+        ++i;
+    }
     return i;
 }
 
